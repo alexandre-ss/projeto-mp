c223ba4a5dbacb4604200810cffbd408dda83486
arthur <arthur.lacava01@gmail.com>
2022-04-23 13:43:36 -0300

    rspec and factorybot configs

---
+++ spec/models/song_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Song, type: :model do
+  context 'check Song' do 
+    it 'when the song is ok' do
+      
+    end
+
+    it 'when the song is not ok' do
+
+    end
+  end
+end
